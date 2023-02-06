ebb7d5107c73025ddb287d895bf9d799ced64cd5
alenastern <alena7690@gmail.com>
2023-02-06 17:27:59 -0500

    add staging files

---
+++ render.R
@@ -0,0 +1,10 @@
+library(rmarkdown)
+library(purrr)
+
+site_pages <- c("infrastructure-funds.Rmd", 
+                "recovery-funds.Rmd", 
+                "FAQ.Rmd", 
+                "glossary.Rmd",
+                "index.Rmd")
+
+walk(site_pages, ~rmarkdown::render(.x, "html_document"))
