acda08a44d70e0d6944b62624b443974f880d398
alenastern <alena7690@gmail.com>
2023-07-28 18:37:28 -0400

    update index

--- render.R
+++ render.R
@@ -1,10 +1,14 @@
 library(rmarkdown)
 library(purrr)
 
-site_pages <- c("infrastructure-funds.Rmd", 
-                "recovery-funds.Rmd", 
-                "FAQ.Rmd", 
+site_pages <- c("infrastructure-funds.Rmd",
+                "recovery-funds.Rmd",
+                "FAQ.Rmd",
                 "glossary.Rmd",
                 "index.Rmd")
+
+
+# site_pages <- c("infrastructure-funds.Rmd", 
+#                 "index.Rmd")
 
 walk(site_pages, ~rmarkdown::render(.x, "html_document"))
