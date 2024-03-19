840cd27fbd8a7bafcb5d8a0e9e2c6da3f2ee8280
Manuel Alcalá Kovalski <malcalakovalski@urban.org>
2024-03-19 12:35:43 -0400

    clean intermediate files that are created during rendering

--- render.R
+++ render.R
@@ -1,10 +1,12 @@
 library(rmarkdown)
 library(purrr)
 
-site_pages <- c("infrastructure-funds.Rmd", 
-                "recovery-funds.Rmd", 
-                "FAQ.Rmd", 
-                "glossary.Rmd",
-                "index.Rmd")
+site_pages <- c(
+  "infrastructure-funds.Rmd",
+  "recovery-funds.Rmd",
+  "FAQ.Rmd",
+  "glossary.Rmd",
+  "index.Rmd"
+)
 
-walk(site_pages, ~rmarkdown::render(.x, "html_document"))
+walk(site_pages, ~ rmarkdown::render(.x, "html_document"), clean = TRUE)
